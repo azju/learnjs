8e4a36f8a452d6c10b4bdb309b2d70eafe665272
Zhongyu Ju <zju@Zhongyus-MacBook-Pro.local>
2021-03-30 02:09:21 +1100

    timer

---
+++ timer_async.js
@@ -0,0 +1,19 @@
+const timerGame = (callback) => {
+  console.log("Ready ... go!");
+  setTimeout(() => {
+    console.log("Time is up -- stop!");
+    callback && callback();
+  }, 1000);
+};
+
+const infiniteTimerGame = (callback) => {
+  console.log("ready go");
+  setTimeout(() => {
+    console.log("Time up! 10 seconds before the next game starts...");
+    callback && callback();
+
+    setTimeout(() => {
+      infiniteTimerGame(callback);
+    }, 10000);
+  }, 1000);
+};
